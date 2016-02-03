363eb0780ce3b9cbea633b5fe3e0f0c728dafc28
Peeter Pirn <pirn@fastmail.fm>
2016-02-03 13:15:29 -0500

    Modify comments in clean.sh

--- clean.sh
+++ clean.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env bash
-
+# 
+# Remove files that need not be added to the repository
+#
 # Remove all backup files created by gedit, which have a `~' at the end of the filename.
 find . -type f -name "*~" -exec rm {} +
-# README.html is generated from README.md and need not be added to the repository.
+# `.html' files are generated from `.md' files and need not be added to the repository.
 find . -type f -name "*.html" -exec rm {} +
