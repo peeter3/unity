cb4705010c6a75042684d55d3a1aef82b7d5969f
Peeter Pirn <pirn@fastmail.fm>
2018-09-09 07:51:15 -0400

    Update clean script to clean up .bak files.

--- clean.sh
+++ clean.sh
@@ -4,6 +4,8 @@
 #
 # Remove all backup files created by gedit, which have a `~' at the end of the filename.
 find . -type f -name "*~" -exec rm {} +
+# Remove all backup files created by copying a file and appending `.bak' to it.
+find . -type f -name "*.bak" -exec rm {} +
 # `.html' files are generated from `.md' files and need not be added to the repository.
 # However, in the `liturgy` directory, there are `.html` files that are not generated;
 # we want to keep these.
